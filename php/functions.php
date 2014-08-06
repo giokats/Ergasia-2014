f65355806d2ad0e9e9a69f041e32a366389fd425
Vasilis-Thanos Anagnostopoulos <anagnwstopoulos@hotmail.com>
2014-08-06 15:36:17 +0300

    Adding some php function and the credentials for the db

---
+++ php/functions.php
@@ -0,0 +1,37 @@
+<?php // functions.php
+
+  require_once 'mysql_login.php';
+  $connection = new mysqli($db_hostname, $db_username, $db_password, $db_database);
+
+  if ($connection->connect_error) die($connection->connect_error);
+
+  //Γιατί βαριέμαι να γράφω πολλά καθε φορά και επειδή μπορώ ...
+  function queryMysql($query)
+  {
+    global $connection; //use of the global variable
+    $result = $connection->query($query);
+    if (!$result) die($connection->error);
+    return $result;
+  }
+
+  function destroySession()
+  {
+    $_SESSION=array();
+
+    if (session_id() != "" || isset($_COOKIE[session_name()]))
+      setcookie(session_name(), '', time()-2592000, '/');
+
+    session_destroy();
+  }
+
+  // A function for sanitazing input for the mysql
+  function sanitizeString($var)
+  {
+    global $connection;
+    $var = strip_tags($var);
+    $var = htmlentities($var);
+    $var = stripslashes($var);
+    return $connection->real_escape_string($var);
+  }
+
+?>
---
+++ php/mysql_login.php
@@ -0,0 +1,9 @@
+<?php //mysql_login.php';)
+  // The credentials for the mysql database
+  $db_hostname = 'localhost';
+  $db_database = 'tme121db';
+  $db_username = 'tme121';
+  $db_password = 'tme121_password';
+?>
+
+
